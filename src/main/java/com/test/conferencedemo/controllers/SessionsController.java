f1595af6749ad436a1c72dfb47e2ddfea50bb02d
Aakash Jain <aakash.jain01@sap.com>
2022-03-07 18:54:01 +0530

    repository and models created

---
+++ src/main/java/com/test/conferencedemo/controllers/SessionsController.java
@@ -0,0 +1,33 @@
+package com.test.conferencedemo.controllers;
+
+import com.test.conferencedemo.models.Session;
+import com.test.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class SessionsController {
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list() {
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable Long id){
+        return sessionRepository.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+}
