42bb1570760cde6c468009b91278dbffe3a17ebf
Aakash Jain <aakash.jain01@sap.com>
2022-03-14 14:00:27 +0530

    completed module

---
+++ src/main/java/com/test/conferencedemo/config/persistenceConfig.java
@@ -0,0 +1,20 @@
+package com.test.conferencedemo.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class persistenceConfig {
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        System.out.println("Custom data source");
+        return builder.build();
+    }
+
+
+}
